66be85707ee505d3884193e025a6c2c83d4a7f7c
kharneit <126403256+kharneit@users.noreply.github.com>
2023-08-23 22:59:41 +0200

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,6 @@
+from sys import argv
+"""
+Handling console arguments and instanciating the main object
+"""
+if __name__ == '__main__':
+    raise NotImplementedError('This functionality has not been implemented yet.')
